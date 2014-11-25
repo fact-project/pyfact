0e28f04b2bbaf1866327061e4707c149dc2ffbe3
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2014-11-25 23:40:39 +0100

    fix window resizing and button presses

--- PyFactViewer.py
+++ PyFactViewer.py
@@ -28,11 +28,11 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import tkinter as Tk
 import numpy as np
-import matplotlib.pyplot as plt
 
 class Viewer():
     def __init__(self, dataset, key, pixelset=None, mapfile="pixel-map.csv", cmap="gray"):
         self.event = 0
+        self.resizing = False
         self.dataset = dataset
         self.numEvents = len(dataset)
         self.pixelset = pixelset
@@ -40,8 +40,6 @@ class Viewer():
         self.cmap = cmap
         self.pixel_x, self.pixel_y = self.get_pixel_coords(mapfile)
         self.fig = Figure(figsize=(7,6), dpi=100)
-        self.figheight = self.fig.get_figheight()
-        self.figwidth = self.fig.get_figwidth()
 
         self.init_plot()
 
@@ -50,6 +48,7 @@ class Viewer():
         self.root = Tk.Tk()
         self.root.geometry(("1024x768"))
         self.root.wm_title("PyFactViewer")
+
 
         buttonFrame = Tk.Frame(self.root)
         plotFrame = Tk.Frame(self.root)
@@ -62,28 +61,32 @@ class Viewer():
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
         self.canvas._tkcanvas.pack(side=Tk.BOTTOM, fill=Tk.BOTH, expand=1)
-        self.canvas.mpl_connect('key_press_event', self.on_key_event)
+        self.root.bind('<Key>', self.on_key_event)
 
         self.quit_button = Tk.Button(master=buttonFrame, text='Quit', command=self.quit, expand=None)
         self.next_button = Tk.Button(master=buttonFrame, text='Next', command=self.next, expand=None)
-        self.redraw_button = Tk.Button(master=buttonFrame, text='Redraw', command=self.redraw, expand=None)
+        # self.redraw_button = Tk.Button(master=buttonFrame, text='Redraw', command=self.redraw, expand=None)
         self.previous_button = Tk.Button(master=buttonFrame, text='Previous', command=self.previous, expand=None)
 
         self.eventstring = Tk.StringVar()
         self.eventstring.set("EventNum: {:05d}".format(self.event))
-        self.eventbox = Tk.Label(buttonFrame, textvariable=self.eventstring)
+        self.eventbox = Tk.Label(master=buttonFrame, textvariable=self.eventstring)
         self.eventbox.pack(side=Tk.LEFT)
         self.previous_button.pack(side=Tk.LEFT)
         self.next_button.pack(side=Tk.LEFT)
-        self.redraw_button.pack(side=Tk.LEFT)
+        # self.redraw_button.pack(side=Tk.LEFT)
         self.quit_button.pack(side=Tk.RIGHT)
+        # self.root.bind("<Configure>", self.store_event)
+        self.root.after(100, self.redraw)
         Tk.mainloop()
-        self.redraw()
+
+    # def store_event(self, event):
+    #     self.lastevent = event
 
     def init_plot(self):
         self.width, self.height = self.fig.get_figwidth(), self.fig.get_figheight()
         self.fig.clf()
-        self.ax = self.fig.add_subplot(1, 1, 1, aspect=1)
+        self.ax = self.fig.add_axes((0.01, 0.01, 0.98, 0.98), aspect=1)
         self.ax.set_xticks([])
         self.ax.set_yticks([])
         self.ax.set_xlim(-200, 200)
@@ -109,11 +112,11 @@ class Viewer():
                                                 marker='H',
                                                 s=self.size,
                                                 cmap=self.cmap)
-        self.cb = self.fig.colorbar(self.plot, ax=self.ax, label=self.key)
+        self.cb = self.fig.colorbar(self.plot, ax=self.ax, label=self.key, shrink=0.95)
         self.cb.set_clim(0, max(self.dataset[self.event]))
         # self.cb.set_ticks(np.arange(0, int(max(self.dataset[self.event])+1)))
         self.cb.draw_all()
-        self.fig.tight_layout()
+        # self.fig.tight_layout()
 
     def calc_marker_size(self):
         bbox = self.ax.get_window_extent().transformed(self.fig.dpi_scale_trans.inverted())
@@ -122,16 +125,17 @@ class Viewer():
         self.linewidth = min(width/1.2, height)/5 * 1.2
 
     def redraw(self):
-        # if self.fig.get_figheight() != self.figheight or self.fig.get_figwidth() != self.figwidth:
-        #     self.init_plot()
-        #     self.canvas.draw()
-        self.calc_marker_size()
-        self.plot._sizes = np.ones(self.dataset.shape[1])*self.size
-        self.plot.set_linewidth(self.linewidth)
-        if self.pixelset is not None:
-            self.pixelsetplot._sizes = np.ones(self.dataset.shape[1])*self.size
-            self.pixelsetplot.set_linewidth(self.linewidth)
-        self.canvas.draw()
+        if self.width != self.fig.get_figwidth() and self.height != self.fig.get_figheight():
+            self.resizing = True
+            self.calc_marker_size()
+            self.plot._sizes = np.ones(self.dataset.shape[1])*self.size
+            self.plot.set_linewidth(self.linewidth)
+            if self.pixelset is not None:
+                self.pixelsetplot._sizes = np.ones(self.dataset.shape[1])*self.size
+                self.pixelsetplot.set_linewidth(self.linewidth)
+            self.canvas.draw()
+            self.resizing = False
+        self.root.after(100, self.redraw)
 
     def get_pixel_coords(self, mapfile):
         softID, chid, pixel_x_soft, pixel_y_soft = np.loadtxt(mapfile,
@@ -166,14 +170,12 @@ class Viewer():
         self.update()
 
     def on_key_event(self, event):
-        if event.key == "right":
+        if event.keysym == "Right":
             self.next()
-        elif event.key == "left":
+        elif event.keysym == "Left":
             self.previous()
-        elif event.key == "q":
+        elif event.keysym == "q":
             self.quit()
-        # key_press_handler(event, canvas, toolbar)
-        self.update()
 
     def update(self):
         self.plot.set_array(self.dataset[self.event])
@@ -199,13 +201,15 @@ if __name__ == "__main__":
     else:
         group = inputfile[args["--group"]]
 
+    for key in sorted(group.keys()):
+        print(key)
+
     dataset = group.get(args["--dataset"])
 
     if args["--pixelset"] is not None:
         pixelset = group.get(args["--pixelset"])
     else:
         pixelset = None
-
 
     myViewer = Viewer(dataset,
                       args["--dataset"],
