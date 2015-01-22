46bdf1a9eb9226f978436f503e119606f8140665
Dominik Neise <dominik.neise@udo.edu>
2015-01-22 17:21:38 +0100

    linted the filler, still tidying up

--- slow_data_db/fill_in.py
+++ slow_data_db/fill_in.py
@@ -7,7 +7,7 @@ Usage:
 Options:
   -h --help          Show this screen.
   --delete_all       delete all collections prior to filling [default: False].
-  --base PATH        base path to slow files (e.g. /fact/aux) [default: /data/fact_aux]
+  --base PATH        base path to slow files [default: /data/fact_aux]
 
 """
 from docopt import docopt
@@ -17,139 +17,167 @@ import pyfact
 from glob import glob
 import time
 import pymongo
-from pprint import pprint
 import settings
-import unittest
+
 
 class MyDate(object):
-    def __init__(self, y, m, d):
-        self.y = y
-        self.m = m
-        self.d = d
+    """ very bad designed Date class
+
+    TODO:
+    exchange the use of this class everywhere in the code agains
+    the use of date.dateime or time-tuple
+    """
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+    def __str__(self):
+        return str(self.year, self.month, self.day)
 
 
-def date_from_path(p):
-    yyyy = int(p[15:19])
-    mm = int(p[20:22])
-    dd = int(p[23:25])
-    return MyDate(yyyy, mm, dd)
+def date_from_path(path):
+    """ return MyDate object generated from a pathname, very wrongly implemented
+    TODO:
+    the path should be taken apart correctly and then the 
+    filename should be parsed into a datetime.datetime object or a time-tuple
+    """
+    year = int(path[15:19])
+    month = int(path[20:22])
+    day = int(path[23:25])
+    return MyDate(year, month, day)
 
 
-def get_mongo_db_collection_by_name(db, name):
+def get_mongo_db_collection_by_name(database, collection_name):
     """ creates collection if it does not yet exist.
         if it exists, it simply returns it.
     """
-    c = getattr(db, name)
-    return c
+    return getattr(database, collection_name)
 
 
 def list_of_services_of_interest():
-    aux_files_of_interest = """
-        AGILENT_CONTROL_24V_DATA
-        AGILENT_CONTROL_50V_DATA
-        AGILENT_CONTROL_80V_DATA
-        BIAS_CONTROL_CURRENT
-        BIAS_CONTROL_DAC
-        BIAS_CONTROL_STATE
-        BIAS_CONTROL_VOLTAGE
-        DRIVE_CONTROL_POINTING_POSITION
-        DRIVE_CONTROL_SOURCE_POSITION
-        DRIVE_CONTROL_STATE
-        DRIVE_CONTROL_STATUS
-        DRIVE_CONTROL_TRACKING_POSITION
-        FAD_CONTROL_CONNECTIONS
-        FAD_CONTROL_DAC
-        FAD_CONTROL_DNA
-        FAD_CONTROL_DRS_RUNS
-        FAD_CONTROL_EVENTS
-        FAD_CONTROL_FILE_FORMAT
-        FAD_CONTROL_FIRMWARE_VERSION
-        FAD_CONTROL_INCOMPLETE
-        FAD_CONTROL_PRESCALER
-        FAD_CONTROL_REFERENCE_CLOCK
-        FAD_CONTROL_REGION_OF_INTEREST
-        FAD_CONTROL_RUN_NUMBER
-        FAD_CONTROL_RUNS
-        FAD_CONTROL_START_RUN
-        FAD_CONTROL_STATE
-        FAD_CONTROL_STATISTICS1
-        FAD_CONTROL_STATS
-        FAD_CONTROL_STATUS
-        FAD_CONTROL_TEMPERATURE
-        FAD_CONTROL_TRIGGER_COUNTER
-        FEEDBACK_CALIBRATED_CURRENTS
-        FEEDBACK_CALIBRATION
-        FEEDBACK_CALIBRATION_R8
-        FEEDBACK_CALIBRATION_STEPS
-        FEEDBACK_STATE
-        FSC_CONTROL_BIAS_TEMP
-        FSC_CONTROL_CURRENT
-        FSC_CONTROL_HUMIDITY
-        FSC_CONTROL_TEMPERATURE
-        FSC_CONTROL_VOLTAGE
-        FTM_CONTROL_COUNTER
-        FTM_CONTROL_DYNAMIC_DATA
-        FTM_CONTROL_FTU_LIST
-        FTM_CONTROL_STATE
-        FTM_CONTROL_STATIC_DATA
-        FTM_CONTROL_TRIGGER_RATES
-        GPS_CONTROL_NEMA
-        LID_CONTROL_DATA
-        LID_CONTROL_STATE
-        MAGIC_WEATHER_DATA
-        MAGIC_WEATHER_STATE
-        MCP_CONFIGURATION
-        MCP_STATE
-        PWR_CONTROL_DATA
-        PWR_CONTROL_STATE
-        RATE_CONTROL_STATE
-        RATE_CONTROL_THRESHOLD
-        RATE_SCAN_DATA
-        RATE_SCAN_PROCESS_DATA
-        RATE_SCAN_STATE
-        SQM_CONTROL_DATA
-        TEMPERATURE_DATA
-        TIME_CHECK_OFFSET
-        TNG_WEATHER_DATA
-        TNG_WEATHER_DUST
-        TNG_WEATHER_STATE
+    """ return a list of service names, which are of interest for us
     """
-    service_names_of_interest = aux_files_of_interest.split()
-    return service_names_of_interest
+    return ['AGILENT_CONTROL_24V_DATA',
+     'AGILENT_CONTROL_50V_DATA',
+     'AGILENT_CONTROL_80V_DATA',
+     'BIAS_CONTROL_CURRENT',
+     'BIAS_CONTROL_DAC',
+     'BIAS_CONTROL_STATE',
+     'BIAS_CONTROL_VOLTAGE',
+     'DRIVE_CONTROL_POINTING_POSITION',
+     'DRIVE_CONTROL_SOURCE_POSITION',
+     'DRIVE_CONTROL_STATE',
+     'DRIVE_CONTROL_STATUS',
+     'DRIVE_CONTROL_TRACKING_POSITION',
+     'FAD_CONTROL_CONNECTIONS',
+     'FAD_CONTROL_DAC',
+     'FAD_CONTROL_DNA',
+     'FAD_CONTROL_DRS_RUNS',
+     'FAD_CONTROL_EVENTS',
+     'FAD_CONTROL_FILE_FORMAT',
+     'FAD_CONTROL_FIRMWARE_VERSION',
+     'FAD_CONTROL_INCOMPLETE',
+     'FAD_CONTROL_PRESCALER',
+     'FAD_CONTROL_REFERENCE_CLOCK',
+     'FAD_CONTROL_REGION_OF_INTEREST',
+     'FAD_CONTROL_RUNS',
+     'FAD_CONTROL_RUN_NUMBER',
+     'FAD_CONTROL_START_RUN',
+     'FAD_CONTROL_STATE',
+     'FAD_CONTROL_STATISTICS1',
+     'FAD_CONTROL_STATS',
+     'FAD_CONTROL_STATUS',
+     'FAD_CONTROL_TEMPERATURE',
+     'FAD_CONTROL_TRIGGER_COUNTER',
+     'FEEDBACK_CALIBRATED_CURRENTS',
+     'FEEDBACK_CALIBRATION',
+     'FEEDBACK_CALIBRATION_R8',
+     'FEEDBACK_CALIBRATION_STEPS',
+     'FEEDBACK_STATE',
+     'FSC_CONTROL_BIAS_TEMP',
+     'FSC_CONTROL_CURRENT',
+     'FSC_CONTROL_HUMIDITY',
+     'FSC_CONTROL_TEMPERATURE',
+     'FSC_CONTROL_VOLTAGE',
+     'FTM_CONTROL_COUNTER',
+     'FTM_CONTROL_DYNAMIC_DATA',
+     'FTM_CONTROL_FTU_LIST',
+     'FTM_CONTROL_STATE',
+     'FTM_CONTROL_STATIC_DATA',
+     'FTM_CONTROL_TRIGGER_RATES',
+     'GPS_CONTROL_NEMA',
+     'LID_CONTROL_DATA',
+     'LID_CONTROL_STATE',
+     'MAGIC_WEATHER_DATA',
+     'MAGIC_WEATHER_STATE',
+     'MCP_CONFIGURATION',
+     'MCP_STATE',
+     'PWR_CONTROL_DATA',
+     'PWR_CONTROL_STATE',
+     'RATE_CONTROL_STATE',
+     'RATE_CONTROL_THRESHOLD',
+     'RATE_SCAN_DATA',
+     'RATE_SCAN_PROCESS_DATA',
+     'RATE_SCAN_STATE',
+     'SQM_CONTROL_DATA',
+     'TEMPERATURE_DATA',
+     'TIME_CHECK_OFFSET',
+     'TNG_WEATHER_DATA',
+     'TNG_WEATHER_DUST',
+     'TNG_WEATHER_STATE']
 
 
-def try_to_delete_all_collections_from(database):
+def delete_all_collections_from(database, omit=('system.indexes')):
+    """ delete all collections from a database
+
+        if name of collection is in omit (default:['system.indexes']), 
+        then this collection is not deleted.
+    """
     for coll_name in database.collection_names():
-        if 'system.indexes' in coll_name:
+        if coll_name in omit:
             continue
+
         coll = get_mongo_db_collection_by_name(database, coll_name)
-        """try:
+        try:
             coll.drop()
-        except pymongo.errors.OperationFailure as e:
+        except pymongo.errors.OperationFailure as exeption:
             print ("Was not able to drop collection {}\n"
                     "Exception Details:\n"
-                    "args:{ex.args} | details:{ex.details} | message:{ex.message} | code:{ex.code}").format(coll_name, ex=e)
-        """
+                    "args:{ex.args} | details:{ex.details} "
+                    "| message:{ex.message} | code:{ex.code}"
+                  ).format(coll_name, ex=exeption)
+            
+
+def service_name_from_path(path):
+    """Grab the service name from a fits_file_path
+
+    path : should be a full file path
+    the path is split and then the filename is taken from it by taking characters [9:-5]
+    this is a vety bad way to implement this!
+    TODO: improve... what if the filename does not look like this: 
+        yyyymmdd.SERVICE_NAME.fits, 
+    but there is a fits.gz or fits.fz ending?? hu?
+    """
+    filename = os.path.split(path)[1]
+    return filename[9:-5]
 
 
-def service_name_from_path(p):
-    filename = os.path.split(p)[1]
-    service_name = filename[9:-5]
-    return service_name
-
-
-def build_mongo_document_from_current_fits_file_row(fits_file):
+def create_mongo_doc(fits_row):
+    """ create a document suitable for mongo DB insertion from a fits_file row
+    """
     doc = dict()
-    for col_name in fits_file.cols:
-        cell_content = fits_file.cols[col_name]
+    for field_name in fits_row:
+        cell_content = fits_row[field_name]
 
         # mongo document fields can contain lists, or scalars, no numpy arrays!
-        # fits files only contain 1D numpy arrays (sometimes with only 1 element)
-        # so here I convert them to lists, and in the 1 element case, to scalars.
+        # fits files only contain 1D numpy arrays 
+        # (sometimes with only 1 element)
+        # so here I convert them to lists, 
+        # and in the 1 element case, to scalars.
         if len(cell_content) > 1:
-            doc[col_name] = cell_content.tolist()
+            doc[field_name] = cell_content.tolist()
         elif len(cell_content) == 1:
-            doc[col_name] = float(cell_content[0])
+            doc[field_name] = float(cell_content[0])
         elif len(cell_content) == 0:
             # pass if we find an empty numpy array, we ignore it.
             pass
@@ -160,8 +188,10 @@ def build_mongo_document_from_current_fits_file_row(fits_file):
     return doc
 
 
-def make_time_index_for_service(fits_file_path, db):
-    """ This simply ensures, that for the collection associated with the dim service,
+def make_time_index_for_service(fits_file_path, database):
+    """ ensure that Time index exists 
+
+        This simply ensures, that for the collection associated with the dim service,
         which is stored inside the given file, there is indeed an index built for the 'Time' field.
 
         So in almost all cases, when this method is called nothing will happen, because the index is already there.
@@ -169,14 +199,16 @@ def make_time_index_for_service(fits_file_path, db):
         This method only triggers an action on the DB side, when there is a new service being filled in,
         or when for some reason an index was dropped.
     """
-    coll = get_mongo_db_collection_by_name(db, service_name_from_path(fits_file_path))
+    coll = get_mongo_db_collection_by_name(database, 
+                                service_name_from_path(fits_file_path))
     coll.ensure_index([("Time", 1)], unique=True, dropDups=True)
 
-
-def bulk_insert_fits_file_to_collection(fits_file, coll):
-    bulk = coll.initialize_ordered_bulk_op()
+def bulk_insert_fits_file(fits_file, collection):
+    """ insert contents of fits_file into collection use bulk insert
+    """
+    bulk = collection.initialize_ordered_bulk_op()
     for row in fits_file:
-        doc = build_mongo_document_from_current_fits_file_row(fits_file)
+        doc = create_mongo_doc(row)
         bulk.insert(doc)
     try:
         bulk.execute()
@@ -184,43 +216,52 @@ def bulk_insert_fits_file_to_collection(fits_file, coll):
         code = bwe.details['writeErrors'][0]['code']
         if not code == 11000:
             raise bwe
-        #pprint(bwe.details)
 
-
-def insert_fits_file_to_collection(fits_file, coll):
+def insert_fits_file(fits_file, coll):
+    """ insert contents of fits_file into collection, SLOW!
+    """
     for row in fits_file:
-        doc = build_mongo_document_from_current_fits_file_row(fits_file)
+        doc = create_mongo_doc(row)
         coll.insert(doc)
 
 
-def insert_service_from_fitsfile_into_db(fits_file_path, db):
+def insert_fitsfile(fits_file_path, database):
+    """ insert data from fits file into the database
+    """
     fits_file = pyfact.Fits(fits_file_path)
-    coll = get_mongo_db_collection_by_name(db, service_name_from_path(fits_file_path))
-    bulk_insert_fits_file_to_collection(fits_file, coll)
-    #insert_fits_file_to_collection(fits_file, coll)
+    collection = get_mongo_db_collection_by_name(database, 
+        service_name_from_path(fits_file_path))
+    bulk_insert_fits_file(fits_file, collection)
+
 
 
 def get_report(fits_file_path, starttime):
+    """ creates a textual report about the time
+    """
     fits_file = pyfact.Fits(fits_file_path)
-    report = "{time_str} : {d.y}/{d.m}/{d.d}: {svc_name:.<40} : {len:6d} : {dura:2.3f}".format(
+    report = ("{time_str} : {d.y}/{d.m}/{d.d}:"
+              " {svc_name:.<40} : {len:6d} : {dura:2.3f}".format(
         d=date_from_path(fits_file_path),
         svc_name=service_name_from_path(fits_file_path),
         dura=time.time() - starttime,
         len=fits_file.header["NAXIS2"],
         time_str=time.strftime("%Y/%m/%d-%H:%M:%S")
-        )
+        ))
 
     return report
 
 
-def is_interesting_for_slow_database(path_):
+def is_file_intersting(path_):
+    """ check if service in file is interesting
+    """
     service_name = service_name_from_path(path_)
     service_names_of_interest = list_of_services_of_interest()
     if service_name in service_names_of_interest:
         return True
 
 def list_of_paths(base):
-
+    """ generate list of .fits files below this basepath
+    """
     search_path = os.path.join(base, '*/*/*/*.fits')
     all_paths = glob(search_path)
     
@@ -228,14 +269,16 @@ def list_of_paths(base):
     
     for path_ in sorted_paths:
         date = date_from_path(path_)
-        if date.y < 2012:
+        if date.year < 2012:
             raise StopIteration()
         yield path_
 
 
-def insert_service_description_from_fitsfile_into_db(fits_file_path, db):
+def insert_service_description(fits_file_path, database):
+    """ insert fits header information into database
+    """
     service_name = service_name_from_path(fits_file_path)
-    coll = db.service_descriptions
+    coll = database.service_descriptions
 
     # only insert this, if it has never been inserted before
     if coll.find({'_id':service_name}).count():
@@ -257,31 +300,30 @@ def insert_service_description_from_fitsfile_into_db(fits_file_path, db):
 
 
 def main(opts):
+    """ main function of this filler
+    """
     connection = pymongo.MongoClient(settings.host, settings.port)
-    db = getattr(connection, settings.database_name)
+    database = getattr(connection, settings.database_name)
     aux_meta = getattr(connection, 'aux_meta')
 
     if opts['--delete_all']:
-        try_to_delete_all_collections_from(db)
+        delete_all_collections_from(database)
 
     for path in list_of_paths(opts['--base']):
-        if is_interesting_for_slow_database(p):
+        if is_file_intersting(path):
             starttime = time.time()
-            insert_service_description_from_fitsfile_into_db(path, aux_meta)
-            insert_service_from_fitsfile_into_db(path, db)
-            make_time_index_for_service(path, db)
-            print get_report(p, starttime)
+            insert_service_description(path, aux_meta)
+            insert_fitsfile(path, database)
+            make_time_index_for_service(path, database)
+            print get_report(path, starttime)
         #TODO:
-        # Store also the **header** of the fits file to the mongo DB, so the **types** of
+        # Store also the **header** of the fits file to the mongo DB, 
+        # so the **types** of
         # the data in the databse.
-        # The header information **should** be identical for all instances of a certain service
+        # The header information **should** be identical for 
+        # all instances of a certain service
         # so only one header per service is needed.
         # but one can never be sure. So
-
-class Sometests(unittest.TestCase):
-
-    def test_something():
-        pass
 
 if __name__ == "__main__":
     main(program_options)
