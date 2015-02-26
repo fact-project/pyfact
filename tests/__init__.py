0886bbbd31a9750e4c304b8a6180c950c2f431c3
Maximilian Noethe <maximilian.noethe@udo.edu>
2015-02-26 13:57:21 +0100

    add test for plotter

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,3 +11,16 @@ def test2():
 
 def test_tk():
     from matplotlib.backends import _tkagg
+
+def test_cameraplot():
+    import matplotlib.pyplot as plt
+    import fact.plotting
+    from numpy.random import uniform
+    import os
+
+    data = uniform(0, 20, 1440)
+    plt.factcamera(data)
+    plt.savefig("test.pdf")
+
+    assert os.path.isfile("test.pdf")
+    os.remove("test.pdf")
