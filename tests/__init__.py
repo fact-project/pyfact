36fcda7f32ec3638048da658a8529e18e85ff8b6
Maximilian Noethe <maximilian.noethe@udo.edu>
2015-02-26 17:06:00 +0100

    add gui stuff to travis

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,12 +4,9 @@ def test_tk():
     from matplotlib.backends import _tkagg
 
 def test_cameraplot():
-    import matplotlib
-    matplotlib.use('Agg', warn=False, force=True)
     import matplotlib.pyplot as plt
     import fact.plotting
     from numpy.random import uniform
-    import os
 
     data = uniform(0, 20, 1440)
     plt.factcamera(data)
