facd09645e326a3a7ad371300081078778b69eec
Maximilian Noethe <maximilian.noethe@udo.edu>
2015-02-26 16:28:08 +0100

    remove test tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,11 @@
 from nose.tools import raises
 
-# if you want to test if something works:
-def test1():
-    assert 1 == 1
-
-# if you want to be sure that something actually raises an exception:
-@raises(AssertionError)
-def test2():
-    assert 1 == 2
-
 def test_tk():
     from matplotlib.backends import _tkagg
 
 def test_cameraplot():
     import matplotlib
-    matplotlib.use('agg')
+    matplotlib.use('agg', warn=False, force=True)
     import matplotlib.pyplot as plt
     import fact.plotting
     from numpy.random import uniform
@@ -22,7 +13,3 @@ def test_cameraplot():
 
     data = uniform(0, 20, 1440)
     plt.factcamera(data)
-    plt.savefig("test.pdf")
-
-    assert os.path.isfile("test.pdf")
-    os.remove("test.pdf")
