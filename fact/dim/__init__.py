a511e40e238cf578775ee852fcbbdb043173e730
Dominik Neise <dominik.neise@udo.edu>
2015-02-28 13:05:49 +0100

    a little less clutter in the namespace of fact.dim

--- fact/dim/__init__.py
+++ fact/dim/__init__.py
@@ -7,8 +7,8 @@ to the one that is used for C.
 
 _version = '1.3.4'
 
-from .rpcproxy import RpcProxy
 from .sync_calls import dic_sync_info_service, dic_sync_cmnd_service
 from .helpers import dim_service, dim_service_tag
-from .dimc import *
+
 from .dim_servers import Dns
+import dimc
