44fb5dbdcb478d1202fcddebd12e41498ef125f2
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-07-22 03:13:23 +0200

    add night

--- fact/time.py
+++ fact/time.py
@@ -81,6 +81,17 @@ def datestr(datetime_inst):
     return datetime_inst.isoformat("T")
 
 
+def night(timestamp=None):
+    """
+    gives the date for a day change at noon instead of midnight
+    """
+    if timestamp is None:
+        timestamp = datetime.utcnow()
+    if timestamp.hour < 12:
+        timestamp = timestamp - timedelta(days=1)
+    return timestamp
+
+
 def night_integer(timestamp=None):
     """ get the correct night in fact format
         if it is after 0:00, take the date
