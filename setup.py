e9dcc44b45a9ac2eca601c2d16494a30cb8dbb9d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-04-22 17:12:51 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if not platform == 'darwin':
 
 setup(
     name='fact',
-    version='0.3',
+    version='0.4.0',
     description='A module containing useful methods for working with fact',
     url='http://github.com/fact-project/pyfact',
     author='Maximilian Noethe, Dominik Neise',
