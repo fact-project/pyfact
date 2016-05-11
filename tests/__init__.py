3bb8ceaec283221bac72139d15a1fea5857cbcc9
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-05-11 13:59:07 +0200

    fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,14 @@
-from nose.tools import raises
-
 def test_tk():
     from matplotlib.backends import _tkagg
 
+
 def test_cameraplot():
-    import matplotlib.pyplot as plt
-    import fact.plotting
+    from fact.plotting import camera
     from numpy.random import uniform
 
     data = uniform(0, 20, 1440)
-    plt.factcamera(data)
+    camera(data)
+
 
 def test_dim_import():
     import fact.dim
