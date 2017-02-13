3439f708b484b86d62910055055b70c361fde9fa
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-13 13:56:17 +0100

    Also import connect_database

--- fact/factdb/__init__.py
+++ fact/factdb/__init__.py
@@ -1,3 +1,3 @@
-from .database import factdata_db
+from .database import factdata_db, connect_database
 from .utils import get_ontime_by_source, get_ontime_by_source_and_runtype
 from .models import *
