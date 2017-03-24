d1a93ef26f053b3b1526c019c9607af59686dc91
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-03-24 12:56:11 +0100

    Use kwarg for binning in example

--- examples/plot_qla.py
+++ examples/plot_qla.py
@@ -7,7 +7,11 @@ runs = get_qla_data(
     sources=['Mrk 501', '1ES 1959+650', 'Crab', 'Mrk 421'],
 )
 
-qla_results = bin_runs(runs, 20, discard_ontime_fraction=0.9)
+qla_results = bin_runs(
+    runs,
+    bin_width_minutes=20,
+    discard_ontime_fraction=0.9,
+)
 plot_qla(qla_results)
 
 plt.show()
