60c839dd451b97ba10e32a9dbb5dac9b4da544d2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-03-29 13:25:24 +0200

    Add functions to write h5py hdf5

--- fact/io.py
+++ fact/io.py
@@ -24,12 +24,15 @@ allowed_extensions = ('.hdf', '.hdf5', '.h5', '.json', '.jsonl', '.jsonlines', '
 native_byteorder = native_byteorder = {'little': '<', 'big': '>'}[sys.byteorder]
 
 
-def write_data(df, file_path, key='table'):
+def write_data(df, file_path, key='table', hdf_format='pandas'):
 
     name, extension = path.splitext(file_path)
 
     if extension in ['.hdf', '.hdf5', '.h5']:
-        df.to_hdf(file_path, key=key, format='table')
+        if hdf_format == 'pandas':
+            df.to_hdf(file_path, key=key, format='table')
+        else:
+            to_h5py(file_path, key=key)
 
     elif extension == '.json':
         df.to_json(file_path)
@@ -89,6 +92,9 @@ def read_h5py(file_path, key='events', columns=None):
                     df[col + '_{}'.format(i)] = array[:, i]
             else:
                 log.warning('Skipping column {}, not 1d or 2d'.format(col))
+
+        if 'index' in df.columns:
+            df.set_index('index', inplace=True)
 
     return df
 
@@ -193,3 +199,158 @@ def check_extension(file_path, allowed_extensions=allowed_extensions):
     p, extension = path.splitext(file_path)
     if extension not in allowed_extensions:
         raise IOError('Allowed formats: {}'.format(allowed_extensions))
+
+
+def to_h5py(filename, df, key='table', mode='w', dtypes=None, index=True, **kwargs):
+    '''
+    Write pandas dataframe to h5py style hdf5 file
+
+    Parameters
+    ----------
+    filename: str
+        output file name
+    df: pd.DataFrame
+        The data to write out
+    key: str
+        the name for the hdf5 group to hold all datasets, default: table
+    mode: str
+        'w' to overwrite existing files, 'a' to append
+    dtypes: dict
+        if given, a mapping of column names to dtypes for conversion.
+    index: bool
+        If bool, also save the index of the dataframe
+
+    All **kwargs are passed to h5py.create_dataset
+    '''
+    assert mode in ('w', 'a'), 'mode has to be either "a" or "w"'
+
+    array = df.to_records(index=index)
+
+    if dtypes is not None:
+        array = change_recarray_dtype(array, dtypes)
+
+    with h5py.File(filename, mode=mode) as f:
+        initialize_h5py(f, array.dtype, key=key, **kwargs)
+        append_to_h5py(f, array, key=key)
+
+
+def change_recarray_dtype(array, dtypes):
+    '''
+    Change dtypes of recarray columns
+
+    Parameters
+    ----------
+    array: np.recarray
+      The array to modify
+    dtypes: dict
+        if given, a mapping of column names to dtypes for conversion.
+
+    Returns
+    -------
+    array: np.recarray
+        Copy of the original array with changed dtypes
+    '''
+
+    # get a modifieabale list of dtypes
+    dt = array.dtype.descr
+    # create a mapping from column name to index
+    idx = {k: i for i, (k, d) in enumerate(dt)}
+
+    for col, new_type in dtypes.items():
+        dt[idx[col]] = (col, new_type)
+
+    dt = np.dtype(dt)
+
+    return array.astype(dt)
+
+
+def initialize_h5py(f, dtypes, key='events', **kwargs):
+    '''
+    Create a group with name `key` and empty datasets for each
+    entry in dtypes.
+
+    Parameters
+    ----------
+    f: h5py.File
+        the hdf5 file, opened either in write or append mode
+    dtypes: numpy.dtype
+        the numpy dtype object of a record or structured array describing
+        the columns
+    key: str
+        the name for the hdf5 group to hold all datasets, default: data
+
+    All kwargs are passed to h5py create_dataset
+    '''
+    group = f.create_group(key)
+
+    for name in dtypes.names:
+        dtype = dtypes[name]
+        maxshape = [None] + list(dtype.shape)
+        shape = [0] + list(dtype.shape)
+
+        if dtype.base == object:
+            dt = h5py.special_dtype(vlen=str)
+
+        elif dtype.type == np.datetime64:
+            # save dates as ISO string, create dummy date to get correct length
+            dt = np.array(0, dtype=dtype).astype('S').dtype
+
+        else:
+            dt = dtype.base
+
+        group.create_dataset(
+            name,
+            shape=tuple(shape),
+            maxshape=tuple(maxshape),
+            dtype=dt,
+            **kwargs
+        )
+
+    return group
+
+
+def append_to_h5py(f, array, key='events'):
+    '''
+    Append a numpy record or structured array to the given hdf5 file
+    The file should have been previously initialized with initialize_hdf5
+
+    Parameters
+    ----------
+    f: h5py.File
+        the hdf5 file, opened either in write or append mode
+    array: numpy.array or numpy.recarray
+        the numpy array to append
+    key: str
+        the name for the hdf5 group with the corresponding data sets
+    '''
+
+    group = f.get(key)
+
+    for column in array.dtype.names:
+        print(column)
+        dataset = group.get(column)
+
+        n_existing_rows = dataset.shape[0]
+        n_new_rows = array[column].shape[0]
+
+        dataset.resize(n_existing_rows + n_new_rows, axis=0)
+
+        # swap byteorder if not native
+        if array[column].dtype.byteorder not in ('=', native_byteorder, '|'):
+            data = array[column].newbyteorder().byteswap()
+        else:
+            data = array[column]
+
+        if data.dtype.type == np.datetime64:
+            data = data.astype('S')
+
+        print(data.dtype, dataset.dtype)
+
+        if data.ndim == 1:
+            dataset[n_existing_rows:] = data
+
+        elif data.ndim == 2:
+            dataset[n_existing_rows:, :] = data
+
+        else:
+            raise NotImplementedError('Only 1d and 2d arrays are supported at this point')
