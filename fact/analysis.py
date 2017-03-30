76e2b5765d3ece2d06833753b8e24ed1a1e6338e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-03-30 16:59:56 +0200

    Add docstring, consistent variable names

--- fact/analysis.py
+++ fact/analysis.py
@@ -3,22 +3,36 @@ import numpy as np
 import datetime
 
 
-def li_ma_significance(N_on, N_off, alpha=0.2):
-    N_on = np.array(N_on, copy=False, ndmin=1)
-    N_off = np.array(N_off, copy=False, ndmin=1)
+def li_ma_significance(n_on, n_off, alpha=0.2):
+    '''
+    Calculate the Li&Ma significance for given
+    observations data
+
+    Parameters
+    ----------
+    n_on: integer or array like
+        Number of events for the on observations
+    n_off: integer of array like
+        Number of events for the off observations
+    alpha: float
+        Scaling factor for the off observations, for wobble observations
+        this is 1 / number of off regions
+    '''
+    n_on = np.array(n_on, copy=False, ndmin=1)
+    n_off = np.array(n_off, copy=False, ndmin=1)
 
     with np.errstate(divide='ignore', invalid='ignore'):
-        p_on = N_on / (N_on + N_off)
-        p_off = N_off / (N_on + N_off)
+        p_on = n_on / (n_on + n_off)
+        p_off = n_off / (n_on + n_off)
 
-        t1 = N_on * np.log(((1 + alpha) / alpha) * p_on)
-        t2 = N_off * np.log((1 + alpha) * p_off)
+        t1 = n_on * np.log(((1 + alpha) / alpha) * p_on)
+        t2 = n_off * np.log((1 + alpha) * p_off)
 
         ts = (t1 + t2)
         significance = np.sqrt(ts * 2)
 
     significance[np.isnan(significance)] = 0
-    significance[N_on < alpha * N_off] = 0
+    significance[n_on < alpha * n_off] = 0
 
     return significance
 
