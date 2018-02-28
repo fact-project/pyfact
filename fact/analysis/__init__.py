49115a6771e6d0f6a3919a1eb468df87cd2b456b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-02-28 12:21:40 +0100

    Remove functions for source dependent separation, fixes #100

--- fact/analysis/__init__.py
+++ fact/analysis/__init__.py
@@ -2,7 +2,6 @@ from .statistics import li_ma_significance
 from .binning import ontime_binning, qla_binning, groupby_observation_blocks, bin_runs
 
 from .core import calc_run_summary_source_independent, split_on_off_source_independent
-from .core import calc_run_summary_source_dependent, split_on_off_source_dependent
 
 from .source import (
     calc_theta_equatorial,
@@ -18,8 +17,6 @@ __all__ = [
     'groupby_observation_blocks',
     'bin_runs',
     'calc_run_summary_source_independent',
-    'calc_run_summary_source_dependent',
-    'split_on_off_source_dependent',
     'split_on_off_source_independent',
     'calc_theta_equatorial',
     'calc_theta_camera',
